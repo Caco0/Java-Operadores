299e89ff1e2eacd2c03054c78518579401820085
Rafael Dametto <cacotpds@gmail.com>
2023-12-06 15:56:10 -0300

    operadores e repetidores, booleans

--- src/App.java
+++ src/App.java
@@ -1,16 +1,43 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String nomeCompleto = "Linguagem " + "Java";
-        System.out.println(nomeCompleto);
+        // String nomeCompleto = "Linguagem " + "Java";
+        // System.out.println(nomeCompleto);
         
-        String concatenacao = "?";
-        concatenacao = 1+1+1+"1";
-        System.out.println("Concatenacao: "+concatenacao);
-        concatenacao = 1+"1"+1+1;
-        System.out.println("Concatenacao: "+concatenacao);
-        concatenacao = 1+"1"+1+"1";
-        System.out.println("Concatenacao: "+concatenacao);
-        concatenacao = "1"+(1+1+1);
-        System.out.println("Concatenacao: "+concatenacao);
+        // String concatenacao = "?";
+        // concatenacao = 1+1+1+"1";
+        // System.out.println("Concatenacao: "+concatenacao);
+        // concatenacao = 1+"1"+1+1;
+        // System.out.println("Concatenacao: "+concatenacao);
+        // concatenacao = 1+"1"+1+"1";
+        // System.out.println("Concatenacao: "+concatenacao);
+        // concatenacao = "1"+(1+1+1);
+        // System.out.println("Concatenacao: "+concatenacao);
+
+        // int numero  = 5;
+        // System.out.println(- numero);
+        // System.out.println(numero);
+
+        // numero = - numero;
+        // System.out.println(numero);
+        // numero = +numero;
+        // System.out.println(numero);
+        // numero = numero * -1;
+        // System.out.println(numero);
+
+        // incrementando 
+
+        // int num = 5;
+        // num = num +2;
+        // System.out.println(num);
+
+        // num++;
+        // System.out.println(num);
+        // System.out.println(++ num); 
+        // o incremento deve vir antes para poder imprimir ++ ou -- depois a variável 
+
+        boolean variavel =  true;
+        System.out.println(!variavel);
+        variavel = !variavel;
+        System.out.println(variavel);
     }
 }
