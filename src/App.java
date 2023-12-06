c6592f5c4251571ffed874291abc617740d762bd
Rafael Dametto <cacotpds@gmail.com>
2023-12-06 15:34:08 -0300

    concatenação

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nomeCompleto = "Linguagem " + "Java";
+        System.out.println(nomeCompleto);
+        
+        String concatenacao = "?";
+        concatenacao = 1+1+1+"1";
+        System.out.println("Concatenacao: "+concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println("Concatenacao: "+concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println("Concatenacao: "+concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println("Concatenacao: "+concatenacao);
     }
 }
